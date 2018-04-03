de9e486e9d73c9af7b2528b4cc3c154c31e3f720
Jojo Tutor <josaphat@dnamicro.com>
2018-04-03 10:30:09 +0800

    added amd transform module

--- dist/index.js
+++
@@ -1,1 +0,0 @@
-'use strict';var _withErrorHandler=require('./withErrorHandler');var _withErrorHandler2=_interopRequireDefault(_withErrorHandler);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}module.exports.withErrorHandler=_withErrorHandler2.default;
