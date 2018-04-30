52d845350884ddbefd4d1d34c642fdf8088b48cf
Jojo Tutor <jeshrave003@gmail.com>
2018-04-30 08:36:58 +0800

    re build

--- dist/commonjs/withErrorHandler.js
+++ dist/commonjs/withErrorHandler.js
@@ -4,76 +4,24 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _propTypes = require('prop-types');
+var _ErrorHandler = require('./ErrorHandler');
 
-var _propTypes2 = _interopRequireDefault(_propTypes);
+var _ErrorHandler2 = _interopRequireDefault(_ErrorHandler);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-function withErrorHandler(ChildComponent) {
-  return function (_PureComponent) {
-    _inherits(_class2, _PureComponent);
-
-    function _class2() {
-      var _ref;
-
-      var _temp, _this, _ret;
-
-      _classCallCheck(this, _class2);
-
-      for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-        args[_key] = arguments[_key];
-      }
-
-      return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = _class2.__proto__ || Object.getPrototypeOf(_class2)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
-        hasError: false
-      }, _temp), _possibleConstructorReturn(_this, _ret);
-    }
-
-    _createClass(_class2, [{
-      key: 'componentDidCatch',
-      value: function componentDidCatch(error, info) {
-        this.setState({ hasError: true });
-        this.props.onError(error, info);
-      }
-    }, {
-      key: 'render',
-      value: function render() {
-        if (this.state.hasError) {
-          return this.props.errorElement;
-        }
-        return _react2.default.createElement(ChildComponent, this.props);
-      }
-    }]);
-
-    return _class2;
-  }(_react.PureComponent);
-}
-
-withErrorHandler.defaultProps = {
-  onError: function onError() {},
-  errorElement: _react2.default.createElement(
-    'h1',
-    null,
-    'Oh no, Something went wrong! :( '
-  )
-};
-
-withErrorHandler.propTypes = {
-  onError: _propTypes2.default.func,
-  errorElement: _propTypes2.default.element
+var withErrorHandler = function withErrorHandler(ChildComponent, errorElement, onError) {
+  return function (props) {
+    return _react2.default.createElement(
+      _ErrorHandler2.default,
+      { errorElement: errorElement, onError: onError },
+      _react2.default.createElement(ChildComponent, props)
+    );
+  };
 };
 
 exports.default = withErrorHandler;
--- dist/es/withErrorHandler.js
+++ dist/es/withErrorHandler.js
@@ -1,63 +1,14 @@
-import _Object$getPrototypeOf from 'babel-runtime/core-js/object/get-prototype-of';
-import _classCallCheck from 'babel-runtime/helpers/classCallCheck';
-import _createClass from 'babel-runtime/helpers/createClass';
-import _possibleConstructorReturn from 'babel-runtime/helpers/possibleConstructorReturn';
-import _inherits from 'babel-runtime/helpers/inherits';
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import ErrorHandler from './ErrorHandler';
 
-function withErrorHandler(ChildComponent) {
-  return function (_PureComponent) {
-    _inherits(_class2, _PureComponent);
-
-    function _class2() {
-      var _ref;
-
-      var _temp, _this, _ret;
-
-      _classCallCheck(this, _class2);
-
-      for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-        args[_key] = arguments[_key];
-      }
-
-      return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = _class2.__proto__ || _Object$getPrototypeOf(_class2)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
-        hasError: false
-      }, _temp), _possibleConstructorReturn(_this, _ret);
-    }
-
-    _createClass(_class2, [{
-      key: 'componentDidCatch',
-      value: function componentDidCatch(error, info) {
-        this.setState({ hasError: true });
-        this.props.onError(error, info);
-      }
-    }, {
-      key: 'render',
-      value: function render() {
-        if (this.state.hasError) {
-          return this.props.errorElement;
-        }
-        return React.createElement(ChildComponent, this.props);
-      }
-    }]);
-
-    return _class2;
-  }(PureComponent);
-}
-
-withErrorHandler.defaultProps = {
-  onError: function onError() {},
-  errorElement: React.createElement(
-    'h1',
-    null,
-    'Oh no, Something went wrong! :( '
-  )
-};
-
-withErrorHandler.propTypes = {
-  onError: PropTypes.func,
-  errorElement: PropTypes.element
+var withErrorHandler = function withErrorHandler(ChildComponent, errorElement, onError) {
+  return function (props) {
+    return React.createElement(
+      ErrorHandler,
+      { errorElement: errorElement, onError: onError },
+      React.createElement(ChildComponent, props)
+    );
+  };
 };
 
 export default withErrorHandler;
